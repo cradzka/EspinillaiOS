014ee6117822de6f081e3448d40838a5db75d142
Kristina Bueche <TimothyErrickson@Timothys-MacBook-Air.local>
2017-03-06 21:36:54 -0700

    playing with tables and nested UIViewers

---
+++ Project Espinilla/Project Espinilla/FormsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FormsTableViewCell.swift
+//  Project Espinilla
+//
+//  Created by Timothy Errickson on 3/6/17.
+//  Copyright © 2017 Matthew Crepeau. All rights reserved.
+//
+
+import UIKit
+
+class FormsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
