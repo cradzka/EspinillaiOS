2bfa91216a443a82cd78c7589ef0fbeac699e49d
Kristina Bueche <TimothyErrickson@Timothys-MacBook-Air.local>
2017-04-06 16:34:29 -0600

    corrected headers and subsection labels

--- Project Espinilla/Project Espinilla/ReferenceCollectionViewDataSourceAndDelegate.swift
+++ Project Espinilla/Project Espinilla/ReferenceCollectionViewDataSourceAndDelegate.swift
@@ -12,15 +12,17 @@ class ReferenceCollectionViewDataSourceAndDelegate: NSObject, UICollectionViewDa
 
     var sections = 3
     var items: Int
-    var names: Array<String>
+    var sectionNames: Array<String>
     var cellInitializationIndex: Int
     var collectionLayout: UICollectionViewLayout
+    var sectionDictionary: [String: [String]]
     
-    init(numberOfCategories: Int, categoryNameList: Array<String>, collectionLayout: UICollectionViewLayout) {
+    init(numberOfCategories: Int, sectionNameList: Array<String>, sectionDictionary: [String : [String]], collectionLayout: UICollectionViewLayout) {
         self.items = numberOfCategories
-        self.names = categoryNameList
+        self.sectionNames = sectionNameList
         self.cellInitializationIndex = 0
         self.collectionLayout = collectionLayout
+        self.sectionDictionary = sectionDictionary
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection: Int) -> Int {
@@ -32,22 +34,28 @@ class ReferenceCollectionViewDataSourceAndDelegate: NSObject, UICollectionViewDa
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ReferenceCell", for: indexPath) as! ReferenceCell
-        cell.ReferenceLabel.text = names[indexPath.row]
+        
+        let sectionName = self.sectionNames[indexPath.section]
+        let subsectionNames = self.sectionDictionary[sectionName]
+
+        cell.ReferenceLabel.text = subsectionNames?[indexPath.row]
         cell.ReferenceLabel.font = UIFont.init(name: "Gill Sans", size: 17.0)
-        cell.contentView.backgroundColor = UIColor.white
+        cell.contentView.backgroundColor = UIColor.lightGray
         cell.ReferenceCellStack.addArrangedSubview(cell.ReferenceLabel)
         
         cell.ReferenceImageView.image = #imageLiteral(resourceName: "refImage")
         cell.ReferenceCellStack.addArrangedSubview(cell.ReferenceImageView)
-        
+        cell.backgroundColor = UIColor.blue
+        cell.contentView.backgroundColor = UIColor.blue
         cell.contentView.addSubview(cell.ReferenceCellStack)
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind: String, at: IndexPath) -> UICollectionReusableView {
         let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionElementKindSectionHeader, withReuseIdentifier: "initialCollectionViewHeader", for: at) as! ReferenceCollectionReusableViewHeader
-        headerView.headerLabel.text = "HELLO"
+        headerView.headerLabel.text = sectionNames[at.section]
         headerView.headerLabel.font = UIFont.init(name: "Gill Sans", size: 17.0)
         headerView.backgroundColor = UIColor.white
         headerView.addSubview(headerView.headerLabel)
