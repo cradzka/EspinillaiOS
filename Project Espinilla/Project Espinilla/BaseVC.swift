4f9410a734a01aa4cdce6764d6e5ab863e5b8729
mjcrepeau <matthew.mjc@gmail.com>
2017-04-09 15:47:22 -0600

    Still working on the side menu...

---
+++ Project Espinilla/Project Espinilla/BaseVC.swift
@@ -0,0 +1,24 @@
+//
+//  BaseVC.swift
+//  Project Espinilla
+//
+//  Created by Matthew Crepeau on 4/9/17.
+//  Copyright © 2017 Matthew Crepeau. All rights reserved.
+//
+
+import UIKit
+
+class BaseVC: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addSlideMenuButton()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
