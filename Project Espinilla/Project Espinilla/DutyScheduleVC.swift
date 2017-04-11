32355472e0155f9e072d2c87925781738260746b
mjcrepeau <matthew.mjc@gmail.com>
2017-04-11 00:50:17 -0600

    Added a table view with duty schedules to the "Duty Schedule" menu option

--- Project Espinilla/Project Espinilla/DutyScheduleVC.swift
+++ Project Espinilla/Project Espinilla/DutyScheduleVC.swift
@@ -8,12 +8,31 @@
 
 import UIKit
 
-class DutyScheduleVC: BaseViewController {
+class DutyScheduleVC: BaseViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet var tableView: UITableView?
+    var items: [String] = ["4.14.2017: 5pm to 4.15.2017: 5pm", "4.16.2017: 5pm to 4.17.2017: 5pm", "4.20.2017: 5pm to 4.21.2017: 5pm"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         addSlideMenuButton()
-        // Do any additional setup after loading the view.
+        self.tableView?.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = (self.tableView!.dequeueReusableCell(withIdentifier: "cell")!)
+        
+        cell.textLabel?.text = self.items[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You selected cell #\(indexPath.row)!")
     }
     
     override func didReceiveMemoryWarning() {
