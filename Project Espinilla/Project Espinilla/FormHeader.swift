fef944e96a22157f6e5e14bc61072a58195e2d8e
Raj Singh <rsingh@nmt.edu>
2017-04-12 01:35:15 -0600

    ugh

--- Project Espinilla/Project Espinilla/FormHeader.swift
+++ Project Espinilla/Project Espinilla/FormHeader.swift
@@ -10,7 +10,9 @@ import UIKit
 
 class FormHeader: UICollectionReusableView {
 
+    
     @IBOutlet weak var headerLabel: UILabel!
+    
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
