8345f54966426c216a2bdcffc065075ed22b6640
Kristina Bueche <TimothyErrickson@Timothys-MacBook-Air.local>
2017-04-25 19:45:09 -0600

    added Reference class

---
+++ Project Espinilla/Project Espinilla/Reference.swift
@@ -0,0 +1,17 @@
+//
+//  Reference.swift
+//  Project Espinilla
+//
+//  Created by Timothy Errickson on 4/25/17.
+//  Copyright © 2017 Matthew Crepeau. All rights reserved.
+//
+
+import UIKit
+
+class Reference {
+    var name: String!
+    
+    init(name: String) {
+        self.name = name
+    }
+}
