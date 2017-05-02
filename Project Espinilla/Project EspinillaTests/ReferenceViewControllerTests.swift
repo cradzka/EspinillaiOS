e71c6514b3892c09a518834d3cb4b6f41bd128d7
Kristina Bueche <TimothyErrickson@Timothys-MacBook-Air.local>
2017-05-01 22:17:22 -0600

    Reference VC unit test

--- Project Espinilla/Project EspinillaTests/ReferenceViewControllerTests.swift
+++ Project Espinilla/Project EspinillaTests/ReferenceViewControllerTests.swift
@@ -17,6 +17,10 @@ class ReferenceViewControllerTests: XCTestCase {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
+    func testfillsubsectionLists() {
+        XCTAssert(true)
+    }
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
